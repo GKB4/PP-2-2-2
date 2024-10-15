ebb47b2c9e20caa398aa2d46b77d117b3a5b29e7
Igor Ilkov <garikb@inbox.ru>
2024-10-15 11:09:32 +0500

    Add PreProject Task 2-2-2 ver. 3

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
 public class Car {
     private String model;
     private String color;
